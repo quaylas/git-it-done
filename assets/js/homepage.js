73be3df1f044fc9443b8696a67c63f3d969a83b7
quaylas <quayla@cxadvisors.com>
2020-12-09 13:01:36 -0700

    added options to search for repos by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -16,7 +17,17 @@ var formSubmitHandler = function (event) {
     else {
         alert('Please enter a GitHub username.');
     }
-}
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute('data-language');
+    if (language){
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = '';
+    }
+};
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -35,6 +46,23 @@ var getUserRepos = function(user) {
     })
     .catch(function(error){
         alert('Unable to connect to GitHub');
+    });
+};
+
+
+
+var getFeaturedRepos = function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+        }
+        else {
+            alert('Error: ' + response.statusText);
+        }
     });
 };
 
@@ -90,3 +118,4 @@ var displayRepos = function(repos, searchTerm) {
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
